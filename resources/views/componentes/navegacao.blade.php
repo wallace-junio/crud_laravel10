acb7e9269282c6dc49e298f919b2d9c6ffa2fe39
wallace-junio <wallacejunio2014@hotmail.com>
2024-10-05 15:02:42 -0300

    Criando Controler, model e migration

--- resources/views/componentes/navegacao.blade.php
+++ resources/views/componentes/navegacao.blade.php
@@ -14,7 +14,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="{{ ROUTE('produtos.index')}}">
               <span data-feather="shopping-cart"></span>
               Produtos
             </a>
